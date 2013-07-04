033b12d3f7e70cf45a3ba0e0ad05f0090d5fbf8c
estornino <eltigrequehayenti@gmail.com>
2013-07-04 16:51:05 +0000

    Read from file fix

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-var body = fs.fs.readFileSync('index.html');
+var body = fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
   response.send(body);
